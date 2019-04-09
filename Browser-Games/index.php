91b23bd08eaa92a810381ffb13db6fef0ea88651
Dayran <dayran44@gmail.com>
2019-04-08 21:49:32 -0300

    g

---
+++ Browser-Games/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="utf8">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/normalize.css">
+    <script src=""></script>
+    <title>TESVS Game</title>
+</head>
+<body>
+<div id="geral">
+    <div id="menu_top">
+        <nav>
+            <ul id="menu">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Ranking</a></li>
+                <li><a href="#">Fórun</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="pages/sing in/sign_up.html">Sign up</a></li>
+            </ul>
+        </nav>
+    </div>
+    <div id="login"> 
+        <form action="" method="POST" id="form">
+            <label for="Email">Email</label>
+        <input type="text" name="Email" />
+        <label for="Password">Password</label>
+        <input type="password" name="Password" />
+        <input type="submit" value="Login" />
+        </form>
+    </div>
+</div>
+</body>
+</html>
